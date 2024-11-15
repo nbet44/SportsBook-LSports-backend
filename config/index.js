7155dc0a30e90c054d1b280984c840af46a1fd19
xyz003 <it.dev0116@gmail.com>
2024-11-16 01:41:21 +0900

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,17 +1,16 @@
 module.exports = {
-    "DBCONNECT": "mongodb://127.0.0.1:27017/bet777?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin",
-    // "TESTDB": "mongodb://164.92.155.203:27017/bet777",
-    "TESTDB": "mongodb://localhost:27017/bet777",
-    "token": "94840-yL6oCP0lnJl8xM",
+    "DBCONNECT": "mongodb://127.0.0.1:27017/betdb?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin",
+    "TESTDB": "mongodb://localhost:27017/betdb",
+    "token": "xxxxx-xxxxxxxxxxxx",
     "XG_siteId": "",
     "XG_publicKey": "",
     "XG_privateKey": "",
     "siteId": "2002",
     "serverPort": 8000,
     "dir": __dirname,
-    "domain": "https://dev.gambabet.com",
-    "UserName": "dev@amojo.io",
-    "Password": "Iuj276yT2!",
+    "domain": "https://xxx.com",
+    "UserName": "dev@xxx.xx",
+    "Password": "",
     "PreMatchID": 1075,
     "InPlayID": 1076
 }
